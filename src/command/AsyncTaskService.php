b617bf8360b63ec1f52d8b7724325d7e1872b5dc
cathy <1574829575@qq.com>
2022-12-17 16:18:35 +0800

    feature:add file_monitor

--- src/command/AsyncTaskService.php
+++ src/command/AsyncTaskService.php
@@ -2,16 +2,22 @@
 namespace cathy\AsyncTask\command;
 use cathy\AsyncTask\main\AsyncTaskSynchronizer;
 use Exception;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use think\console\Command;
 use think\console\Input;
 use think\console\input\Argument;
 use think\console\Output;
+use think\facade\App;
 use think\facade\Config;
+use Workerman\Lib\Timer;
 use Workerman\Worker;
 use function app;
 
 class AsyncTaskService extends Command
 {
+    protected $lastMtime;
+
     public function configure()
     {
         $this->setName('worker:task')
@@ -53,24 +59,39 @@ class AsyncTaskService extends Command
         } else {
             $port = !empty($option['port']) ? $option['port'] : $configs['port'];
         }
-        if(empty($option['process_number'])){
-            $option['process_number'] = $configs['process_number'];
+
+        if($input->hasOption('daemon')){
+            $option['daemon'] = true;
+        } else {
+            $option['daemon'] = !empty($option['daemon']) ? $option['daemon'] : $configs['daemon'];
         }
+
+        if(empty($option['count'])){
+            $option['count'] = $configs['count'];
+        }
+
         if(empty($option['name'])){
             $option['name'] = $configs['name'];
         }
+
         if(empty($option['reuse_port'])){
-            if(isset($option['reuse_port'])){
-                $option['reuse_port'] = false;
-            }else{
-                $option['reuse_port'] = $configs['reuse_port'];
-            }
+            $option['reuse_port'] = $configs['reuse_port'];
         }
+
         if(empty($option['synchronizer_ttl'])){
             $option['synchronizer_ttl'] = $configs['synchronizer_ttl'];
         }
-        if($input->hasOption('daemon')){
-            $option['daemon'] = true;
+
+        if(empty($option['file_monitor'])){
+            $option['file_monitor'] = $configs['file_monitor'];
+        }
+
+        if(empty($option['file_monitor_interval'])){
+            $option['file_monitor_interval'] = $configs['file_monitor_interval'];
+        }
+
+        if(empty($option['file_monitor_path'])){
+            $option['file_monitor_path'] = $configs['file_monitor_path'];
         }
 
         $this->start($host, (int) $port, $option);
@@ -81,9 +102,32 @@ class AsyncTaskService extends Command
             Worker::$daemonize = true;
         }
         $task_worker = new Worker('tcp://'.$host.':'.$port);
-        $task_worker->count = $option['process_number']; // 进程数
+        $task_worker->count = $option['count']; // 进程数
         $task_worker->name = $option['name']; // 名称
         $task_worker->reusePort = $option['reuse_port']; // 根据配置开启端口复用，让每一个任务进程平衡异步任务，仅php7支持
+        // 设置文件监控
+        if (DIRECTORY_SEPARATOR !== '\\' && App::isDebug() && $option['file_monitor'] && 0 == $task_worker->id) {
+            $timer = $option['file_monitor_interval'] ?: 2;
+            $paths = !empty($option['file_monitor_path']) ? $option['file_monitor_path'] : [App::getAppPath(), App::getConfigPath()];
+            Timer::add($timer, function () use ($paths) {
+                foreach ($paths as $path) {
+                    $dir      = new RecursiveDirectoryIterator($path);
+                    $iterator = new RecursiveIteratorIterator($dir);
+                    foreach ($iterator as $file) {
+                        if (pathinfo($file, PATHINFO_EXTENSION) != 'php') {
+                            continue;
+                        }
+
+                        if ($this->lastMtime < $file->getMTime()) {
+                            echo '[update]' . $file . "\n";
+                            posix_kill(posix_getppid(), SIGUSR1);
+                            $this->lastMtime = $file->getMTime();
+                            return;
+                        }
+                    }
+                }
+            });
+        }
         $task_worker->onMessage = function($connection, $data) use ($option) {
             $data = json_decode($data,true);
             $taskClass = $data['taskClass'];
--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,13 @@
 <?php
 return [
-    'host'             => '0.0.0.0',
-    'port'             => '19345',
-    'process_number'   => 4,
-    'name'             => 'AsyncTask',
-    'reuse_port'       => true,
-    'synchronizer_ttl' => 3600
+    'host'                  => '0.0.0.0',
+    'port'                  => 19345,
+    'count'                 => 4,
+    'name'                  => 'AsyncTask',
+    'daemon'                => true, // 是否开启守护进程
+    'reuse_port'            => true, // 是否开启端口复用
+    'synchronizer_ttl'      => 3600, // 任务同步器taskKey缓存时间
+    'file_monitor'          => true, // 是否开启PHP文件更改监控（调试模式下自动开启）
+    'file_monitor_interval' => 2,    // 文件监控检测时间间隔（秒）
+    'file_monitor_path'     => [],   // 文件监控目录 默认监控application和config目录
 ];
